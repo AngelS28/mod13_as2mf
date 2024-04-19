11f69667c66268ee96bf84d4743b610f07d4e593
AngelS28 <as2mf@umsystem.edu>
2024-04-19 17:05:00 -0500

    update mod13_as2mf.py

--- mod13_as2mf.py
+++ mod13_as2mf.py
@@ -1,66 +1,55 @@
 import datetime 
 
-def get_symbol(symbol):
-    if not symbol.isalpha() or not symbol.isupper() or not (1 <= len(symbol) <= 7):
-        return False
-    else:
-        return True 
-    
-def get_chart_type(chart_type):
-    if chart_type not in ['1', '2']:
-        return False 
-    else:
-        return True
-    
-def get_time_series(time_series):
-    try:
-        time_series = int(time_series)
-        if not (1 <= time_series <= 4):
-            return False
+def get_symbol():
+    while True:
+        symbol = input("Enter the symbol for stock visualizer: ")
+        if symbol.isalpha() and symbol.isupper() and (1 <= len(symbol) <= 7):
+            return symbol
         else:
-            return True
-    except ValueError:
-        return False
+            print("Symbol must be capitalized and consist of 1-7 alpha characters.")
 
-def get_date(date):
-    try:
-        datetime.datetime.strptime(date, '%Y-%m-%d')
-    except ValueError:
-        return False
-    
-    return True
+def get_chart_type():
+    while True:
+        chart_type = input("Enter the chart type for stock visualizer (1 or 2): ")
+        if chart_type in ['1', '2']:
+            return chart_type
+        else:
+            print("Chart type must be 1 or 2.")
+
+def get_time_series():
+    while True:
+        time_series = input("Enter the time series for stock visualizer (1-4): ")
+        try:
+            time_series = int(time_series)
+            if 1 <= time_series <= 4:
+                return time_series
+            else:
+                print("Time series must be between 1 and 4.")
+        except ValueError:
+            print("Please enter a valid integer for time series.")
+
+def get_date(prompt):
+    while True:
+        date = input(prompt)
+        try:
+            datetime.datetime.strptime(date, '%Y-%m-%d')
+            return date
+        except ValueError:
+            print("Date must be in the format YYYY-MM-DD.")
 
 def main():
-    symbol = input("Enter the symbol for stock visualizer: ")
-    chart_type = input("Enter the chart type for stock visualizer: ")
-    time_series = input("Enter the time series for stock visualizer: ")
-    start_date = input("Enter the start date for stock visualizer: ")
-    end_date = input("Enter the end date for stock visualizer: ")
+    try:
+        symbol = get_symbol()
+        chart_type = get_chart_type()
+        time_series = get_time_series()
+        start_date = get_date("Enter the start date for stock visualizer (YYYY-MM-DD): ")
+        end_date = get_date("Enter the end date for stock visualizer (YYYY-MM-DD): ")
 
-    errors = []
-
-    if not get_symbol(symbol):
-        errors.append("Symbol must be capitalized and consist of 1-7 alpha characters.")
-
-    if not get_chart_type(chart_type):
-        errors.append("Chart type must be 1 or 2.")
-    
-    if not get_time_series(time_series):
-        errors.append("Time series must be between 1 and 4.")
-    
-    if not get_date(start_date):
-        errors.append("Start date must be in the format YYYY-MM-DD.")
-
-    if not get_date(end_date):
-        errors.append("End date must be in the format YYYY-MM-DD.")
-
-    if errors:
-        print("Input errors:")
-        for error in errors:
-            print("- ", error)
-
-    else:
         print("Input is valid.")
+    except KeyboardInterrupt:
+        print("\nProgram terminated by user.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
